30bb6bf4c861df7eda38a97bc1c45421cd322374
Róbert A. Jack <robertalex00@hotmail.com>
2025-03-26 23:34:04 +0000

    Gerði Sign-in Dialog-inn og grunn innskáningar virkni

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,5 +6,10 @@ module throunhugbunadar.hotelbokanir {
 
 
     opens throunhugbunadar.hotelbokanir to javafx.fxml;
+    opens throunhugbunadar.hotelbokanir.vidmot to javafx.fxml;
+    opens throunhugbunadar.hotelbokanir.vinnsla to javafx.fxml;
     exports throunhugbunadar.hotelbokanir;
+    exports throunhugbunadar.hotelbokanir.vidmot;
+    exports throunhugbunadar.hotelbokanir.vinnsla;
+
 }

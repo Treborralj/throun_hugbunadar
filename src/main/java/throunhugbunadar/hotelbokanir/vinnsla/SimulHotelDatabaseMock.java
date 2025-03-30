a6ffbe47db0ebef95e70d920116c3d785ae37dcc
Undanemma <112326963+Undanemma@users.noreply.github.com>
2025-03-30 19:40:53 +0000

    lagaði aðeins SimulHotelDatabaseMock

--- src/main/java/throunhugbunadar/hotelbokanir/vinnsla/SimulHotelDatabaseMock.java
+++ src/main/java/throunhugbunadar/hotelbokanir/vinnsla/SimulHotelDatabaseMock.java
@@ -13,8 +13,8 @@ public class SimulHotelDatabaseMock implements HotelDatabase {
     public List<Hotel> findAvailableHotels(String location, String checkIn, String checkOut, boolean pool, boolean gym, boolean bar, String nameOfHotel) {
         List<Hotel> found = new ArrayList<>();
         for (Hotel h : hotels) {
-            if(!h.getStadsetning().isEmpty() && !h.getStadsetning().equals(location)) {continue;}
-            if(!h.getNafn().isEmpty() && !h.getNafn().equals(nameOfHotel)) {continue;}
+            if(!location.isEmpty() && !h.getStadsetning().equals(location)) {continue;}
+            if(!nameOfHotel.isEmpty() && !h.getNafn().equals(nameOfHotel)) {continue;}
             if(pool && !h.hasPool()) {continue;}
             if(gym && !h.hasGym()) {continue;}
             if(bar && !h.hasBar()) {continue;}

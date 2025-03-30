e927db3a7e9335ec1eadfba2a1060c7755509f3a
Róbert A. Jack <robertalex00@hotmail.com>
2025-03-30 18:23:59 +0000

    Test klasinn

--- src/test/java/throunhugbunadar/hotelbokanir/HotelTest.java
+++ src/test/java/throunhugbunadar/hotelbokanir/HotelTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import throunhugbunadar.hotelbokanir.vinnsla.Hotel;
 import throunhugbunadar.hotelbokanir.vinnsla.HotelVinnsla;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,33 +18,72 @@ import static org.junit.jupiter.api.Assertions.*;
  *
  *****************************************************************************/
 public class HotelTest {
-    private MockHotel hotels;
+    private SimulHotelDBMock hotels;
+    private HotelController controller;
 
     @BeforeEach
     void setup(){
-        hotels = new MockHotel();
+        controller = new HotelController;
     }
     @AfterEach
     void teardown(){
+        controller = null;
         hotels = null;
-
     }
     @Test
     void testSearchHotelWithNoParameters(){
-        List<Hotel> results = HotelVinnsla.finnaLausHotel("", "", false, false, false, "");
-        assertFalse(results.isEmpty());
+        List<Hotel >hotelList = new ArrayList<>(List.of(
+                new Hotel(1, "Hotel a", "Reykjavík", 100, true, false, true),
+                new Hotel(2, "Hotel b", "Akureyri", 75, false, true, true),
+                new Hotel(3, "Hotel c", "Selfoss", 125, true, false, false),
+                new Hotel(4, "Hotel d", "Vík", 50, true, true, true),
+                new Hotel(5, "Hotel e", "Eigilsstaðir", 75, false, false, false),
+                new Hotel(6, "Hotel f", "Borgarnes", 80, false, true, false)
+        ));
+        hotels = new SumulHotelDBMock(hotelList);
+        List<Hotel> results = controller.searchHotel(hotels, "", "", "", false, false, false, "");
+        assertTrue(results.isEmpty());
     }
     @Test
-    void testSearchByBarOnly() {
-        List<Hotel> results = HotelVinnsla.finnaLausHotel("", "", true, false, false, "");
-        for (Hotel hotel : results) {
-            assertTrue(hotel.isBar());
+    void testSearchByBar() {
+        List<Hotel >hotelList = new ArrayList<>(List.of(
+                new Hotel(1, "Hotel a", "Reykjavík", 100, true, false, true),
+                new Hotel(2, "Hotel b", "Akureyri", 75, false, true, true),
+                new Hotel(3, "Hotel c", "Selfoss", 125, true, false, false),
+                new Hotel(4, "Hotel d", "Vík", 50, true, true, true),
+                new Hotel(5, "Hotel e", "Eigilsstaðir", 75, false, false, false),
+                new Hotel(6, "Hotel f", "Borgarnes", 80, false, true, false)
+        ));
+        hotels = new SumulHotelDBMock(hotelList);
+        List<Hotel> results = controller.searchHotel(hotels, "", "31-03-2025", "02-04-2025", false, false, true, "");
+        for(Hotel r: results){
+            assertTrue(r.hasBar());
         }
     }
     @Test
-    void testSearchByNameOnly() {
+    void testSearchByName() {
         List<Hotel> results = HotelVinnsla.finnaLausHotel("", "", false, false, false, "Hotel Rey");
         assertTrue(results.stream().anyMatch(h -> h.getNafn().toLowerCase().contains("hotel rey")));
+    }
+    @Test
+    void testSearchWithPartialName(){
+
+    }
+    @Test
+    void testSearchWithWrongName(){
+
+    }
+    @Test
+    void testSearchByLocation(){
+
+    }
+    @Test
+    void testSearchByAllFilters(){
+
+    }
+    @Test
+    void testSearchWithInvalidInput(){
+
     }
 
 }

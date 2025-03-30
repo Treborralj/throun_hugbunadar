7aa3ad8454016f8a73beffa885c7b86701da53e0
Undanemma <112326963+Undanemma@users.noreply.github.com>
2025-03-30 19:35:03 +0000

    setti inn minn hluta

--- src/test/java/throunhugbunadar/hotelbokanir/HotelTest.java
+++ src/test/java/throunhugbunadar/hotelbokanir/HotelTest.java
@@ -31,18 +31,10 @@ public class HotelTest {
         hotels = null;
     }
     @Test
-    void testSearchHotelWithNoParameters(){
-        List<Hotel >hotelList = new ArrayList<>(List.of(
-                new Hotel(1, "Hotel a", "Reykjavík", 100, true, false, true),
-                new Hotel(2, "Hotel b", "Akureyri", 75, false, true, true),
-                new Hotel(3, "Hotel c", "Selfoss", 125, true, false, false),
-                new Hotel(4, "Hotel d", "Vík", 50, true, true, true),
-                new Hotel(5, "Hotel e", "Eigilsstaðir", 75, false, false, false),
-                new Hotel(6, "Hotel f", "Borgarnes", 80, false, true, false)
-        ));
-        hotels = new SumulHotelDBMock(hotelList);
+    void testSearchHotelWithNoDates(){
         List<Hotel> results = controller.searchHotel(hotels, "", "", "", false, false, false, "");
         assertTrue(results.isEmpty());
+        assertTrue(controller.message.equals("Please choose valid check-in and check-out dates"));
     }
     @Test
     void testSearchByBar() {
@@ -51,10 +43,10 @@ public class HotelTest {
                 new Hotel(2, "Hotel b", "Akureyri", 75, false, true, true),
                 new Hotel(3, "Hotel c", "Selfoss", 125, true, false, false),
                 new Hotel(4, "Hotel d", "Vík", 50, true, true, true),
-                new Hotel(5, "Hotel e", "Eigilsstaðir", 75, false, false, false),
+                new Hotel(5, "Hotel e", "Egilsstaðir", 75, false, false, false),
                 new Hotel(6, "Hotel f", "Borgarnes", 80, false, true, false)
         ));
-        hotels = new SumulHotelDBMock(hotelList);
+        hotels = new SimulHotelDBMock(hotelList);
         List<Hotel> results = controller.searchHotel(hotels, "", "31-03-2025", "02-04-2025", false, false, true, "");
         for(Hotel r: results){
             assertTrue(r.hasBar());
@@ -79,11 +71,26 @@ public class HotelTest {
     }
     @Test
     void testSearchByAllFilters(){
-
+        List<Hotel >hotelList = new ArrayList<>(List.of(
+                new Hotel(1, "Hotel a", "Reykjavík", 100, true, false, true),
+                new Hotel(2, "Hotel b", "Akureyri", 75, false, true, true),
+                new Hotel(3, "Hotel c", "Selfoss", 125, true, false, false),
+                new Hotel(4, "Hotel d", "Vík", 50, true, true, true),
+                new Hotel(5, "Hotel e", "Egilsstaðir", 75, false, false, false),
+                new Hotel(6, "Hotel f", "Borgarnes", 80, false, true, false)
+        ));
+        hotels = new SimulHotelDBMock(hotelList);
+        List<Hotel> results = controller.searchHotel(hotels, "", "31-03-2025", "02-04-2025", true,true, true, "");
+        assertEquals(1,results.size());
+        assertEquals(4,results.getFirst().getId());
     }
-    @Test
-    void testSearchWithInvalidInput(){
 
+    @Test
+    void testSearchWithInvalidInputType(){
+        List<Hotel> results = controller.searchHotel(hotels, 153, "31-03-2025", "02-04-2025", "True",true, true,9043);
+
+        assertTrue(controller.message.equals("Please use the specified format for the search options"));
+        assertTrue(results.isEmpty());
     }
 
 }

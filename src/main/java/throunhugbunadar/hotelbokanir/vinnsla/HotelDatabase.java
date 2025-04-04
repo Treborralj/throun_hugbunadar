0edfd5132a00ce997f31bd44f98967a7b7422fae
Róbert A. Jack <robertalex00@hotmail.com>
2025-04-04 12:25:40 +0000

    Eyddi klösum sem voru ekki nauðsinlegir

--- src/main/java/throunhugbunadar/hotelbokanir/vinnsla/HotelDatabase.java
+++
@@ -1,9 +0,0 @@
-package throunhugbunadar.hotelbokanir.vinnsla;
-
-import java.util.List;
-
-public interface HotelDatabase {
-
-    public List<Hotel> findAvailableHotels(String location, String checkIn, String checkOut, boolean pool, boolean gym, boolean bar, String nameOfHotel);
-
-}

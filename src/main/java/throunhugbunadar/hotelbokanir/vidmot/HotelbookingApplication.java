91cebe70eb84b6cfa4ea82bffc07bf163d6d350c
Sævar Axel <sab58@hi.is>
2025-04-07 11:24:45 +0000

    Bætti við CSS

--- src/main/java/throunhugbunadar/hotelbokanir/vidmot/HotelbookingApplication.java
+++ src/main/java/throunhugbunadar/hotelbokanir/vidmot/HotelbookingApplication.java
@@ -12,6 +12,7 @@ public class HotelbookingApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HotelbookingApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 600);
+        scene.getStylesheets().add(getClass().getResource("/css/hello-view.css").toExternalForm());
         stage.setMinWidth(563);
         stage.setMinHeight(800);
         stage.setTitle("Hótelbókun!!");
